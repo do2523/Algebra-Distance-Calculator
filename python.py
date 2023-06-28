313f9e83f8794d2ea3182cc46add44a283bfa38d
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-28 13:46:28 -0400

    Create vars

--- python.py
+++ python.py
@@ -1,5 +1,15 @@
 import math
 
+# distance formula
+
 
 def dist(p1, p2):
     return math.sqrt((p2[1]-p1[1])**2 + (p2[0]-p1[0])**2)
+
+
+def distance(points):
+    # vars
+    minimum_distance = float("inf")
+    p1 = 0
+    p2 = 0
+    # nested for loop to go through each point

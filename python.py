06a411c46995c46997d924681d8678107e620d57
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-28 13:52:02 -0400

    Implement code

--- python.py
+++ python.py
@@ -13,3 +13,14 @@ def distance(points):
     p1 = 0
     p2 = 0
     # nested for loop to go through each point
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            distance = dist(points[i], points[j])
+            if distance < minimum_distance:
+                minimum_distance = distance
+                p1 = points[i]
+                p2 = points[j]
+    return p1, p2, minimum_distance
+
+
+print(distance([(1, 2), (4, 6)]))
